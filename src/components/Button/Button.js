e5a3e6fabec88a4a77086f98b87cc29d4e542e39
Michael Hall <mhalla40@gmail.com>
2020-07-17 12:26:48 -0400

    outline buttons complete

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,25 @@
-import React from 'react';
-
+import React from "react";
+import "./Button.css";
 // This is a functional component - just sent up a little differently as an arrow function!
-const Button = (props) => (
-    <button>
-        {props.label}
-    </button>
-)
+const Button = (props) => {
+  let classList = "";
 
-export default Button; 
+  let types = ["primary", "danger", "success", "warning", "default"];
+
+  if (types.includes(props.type)) {
+    classList += `button-${props.type}`;
+  }
+  if (props.large) {
+    classList += ` button-large`; // Note the spacing here since we are adding to the string!
+  }
+  if (props.outline) {
+    classList += ` button-outline-${props.type}`;
+  }
+
+  return (
+    <button className={classList} onClick={props.whenClicked}>
+      {props.label}
+    </button>
+  );
+};
+export default Button;
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,7 +1,36 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Button from "./Button";
 
-import Button from './Button';
+function onClick() {
+  alert("you clicked me");
+}
 
-storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+storiesOf("Button", module)
+  .add("Primary", () => (
+    <Button type="primary" whenClicked={onClick} label="Primary" />
+  ))
+  .add("Danger", () => <Button type="danger" label="Danger" />)
+  .add("Success", () => <Button type="success" label="Success" />)
+  .add("Warning", () => <Button type="warning" label="Warning" />)
+  .add("Default", () => <Button type="default" label="Default" />)
+
+  .add("Outline Primary", () => (
+    <Button type="primary" outline label="Primary" />
+  ))
+  .add("Outline Danger", () => <Button type="danger" outline label="Danger" />)
+  .add("Outline Success", () => (
+    <Button type="success" outline label="Success" />
+  ))
+  .add("Outline Warning", () => (
+    <Button type="warning" outline label="Warning" />
+  ))
+  .add("Outline Default", () => (
+    <Button type="default" outline label="Default" />
+  ))
+
+  .add("Large Primary", () => <Button type="primary" large label="Primary" />)
+  .add("Large Danger", () => <Button type="danger" large label="Danger" />)
+  .add("Large Success", () => <Button type="success" large label="Success" />)
+  .add("Large Warning", () => <Button type="warning" large label="Warning" />)
+  .add("Large Default", () => <Button type="default" large label="Default" />);
